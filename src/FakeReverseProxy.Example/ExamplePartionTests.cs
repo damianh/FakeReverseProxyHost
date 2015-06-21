54312f31a63115cc98cef017ca986d8e255a965f
Damian Hickey <dhickey@gmail.com>
2015-06-21 22:30:03 +0200

    Split examples out.

--- src/FakeReverseProxy.Example/ExamplePartionTests.cs
+++ src/FakeReverseProxy.Example/ExamplePartionTests.cs
@@ -11,7 +11,7 @@ namespace FakeReverseProxy.Example
     using Xunit;
     using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
 
-    public class Examples
+    public class ExamplePartionTests
     {
         [Fact]
         public async Task ExamplePartion()
@@ -52,45 +52,8 @@ namespace FakeReverseProxy.Example
             }
         }
 
-        [Fact]
-        public async Task ExampleRoundRobin()
-        {
-            AppFunc app1 = CreateApplication("instance1");
-            AppFunc app2 = CreateApplication("instance2");
 
-            var settings = new FakeReverseProxySettings();
-
-            // Here we're round-robining a path to two seperate apps
-            settings.Forward("/path/")
-                .To(app1, new Uri("http://backend1.example.com/pathX/"))
-                .To(app2, new Uri("http://backend2.example.com/pathY/"));
-
-            var reverseProxy = new FakeReverseProxy(settings);
-            var handler = new OwinHttpMessageHandler(reverseProxy.AppFunc)
-            {
-                AllowAutoRedirect = true,
-                UseCookies = true
-            };
-            using (var client = new HttpClient(handler)
-            {
-                BaseAddress = new Uri("http://example.com/")
-            })
-            {
-                // Should hit app1
-                var response = await client.GetAsync("/path/");
-
-                var body = await response.Content.ReadAsStringAsync();
-                body.Should().Be("instance1");
-
-                // Should hit app2
-                response = await client.GetAsync("/path/");
-
-                body = await response.Content.ReadAsStringAsync();
-                body.Should().Be("instance2");
-            }
-        }
-
-        public static AppFunc CreateApplication(string instance)
+        private static AppFunc CreateApplication(string instance)
         {
             var app = new AppBuilder();
             app.Run(ctx => ctx.Response.WriteAsync(instance));
---
+++ src/FakeReverseProxy.Example/ExamplesRoundRobinTests.cs
@@ -0,0 +1,62 @@
+﻿
+namespace FakeReverseProxy.Example
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using FakeReverseProxyHost;
+    using FluentAssertions;
+    using Microsoft.Owin.Builder;
+    using Owin;
+    using Xunit;
+    using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+    public class ExamplesRoundRobinTests
+    {
+        
+        [Fact]
+        public async Task ExampleRoundRobin()
+        {
+            AppFunc app1 = CreateApplication("instance1");
+            AppFunc app2 = CreateApplication("instance2");
+
+            var settings = new FakeReverseProxySettings();
+
+            // Here we're round-robining a path to two seperate apps
+            settings.Forward("/path/")
+                .To(app1, new Uri("http://backend1.example.com/pathX/"))
+                .To(app2, new Uri("http://backend2.example.com/pathY/"));
+
+            var reverseProxy = new FakeReverseProxy(settings);
+            var handler = new OwinHttpMessageHandler(reverseProxy.AppFunc)
+            {
+                AllowAutoRedirect = true,
+                UseCookies = true
+            };
+            using (var client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri("http://example.com/")
+            })
+            {
+                // Should hit app1
+                var response = await client.GetAsync("/path/");
+
+                var body = await response.Content.ReadAsStringAsync();
+                body.Should().Be("instance1");
+
+                // Should hit app2
+                response = await client.GetAsync("/path/");
+
+                body = await response.Content.ReadAsStringAsync();
+                body.Should().Be("instance2");
+            }
+        }
+
+        private static AppFunc CreateApplication(string instance)
+        {
+            var app = new AppBuilder();
+            app.Run(ctx => ctx.Response.WriteAsync(instance));
+            return app.Build();
+        }
+    }
+}
